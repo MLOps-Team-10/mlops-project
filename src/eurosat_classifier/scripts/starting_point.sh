ccfa406dcf7baaa4705d9a58d30cf5cdae964297
Henrychou444 <henry.chou2004@gmail.com>
2026-01-18 00:31:14 +0100

    print more logs

--- src/eurosat_classifier/scripts/starting_point.sh
+++ src/eurosat_classifier/scripts/starting_point.sh
@@ -11,6 +11,7 @@ docker build -f dockerfiles/train.dockerfile -t "${IMAGE_NAME}" .
 docker run --rm \
   --name train \
   --shm-size=2g \
+  -e PYTHONUNBUFFERED=1 \
   -v "$PWD/.secrets/bucket_manager.json:/run/secrets/gcp-sa.json" \
   -e GOOGLE_APPLICATION_CREDENTIALS=/run/secrets/gcp-sa.json \
   "${IMAGE_NAME}"
