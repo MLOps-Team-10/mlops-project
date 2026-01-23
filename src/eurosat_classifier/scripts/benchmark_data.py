8081022e43c5e301d3e32b83729fd7a79aaa5124
Marco <marco.baresi01@gmail.com>
2026-01-23 15:23:34 +0100

    refactor: execute ruff format on the code to make the github actions pass

--- src/eurosat_classifier/scripts/benchmark_data.py
+++ src/eurosat_classifier/scripts/benchmark_data.py
@@ -4,11 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from eurosat_classifier.data import get_dataloaders, DataConfig
 
+
 def benchmark():
     worker_counts = [0, 1, 2, 4, 8, 12]
-    num_runs = 5 
-    batches_to_check = 50 
-    
+    num_runs = 5
+    batches_to_check = 50
+
     final_means = []
     final_stds = []
 
@@ -19,16 +20,17 @@ def benchmark():
         for run in range(num_runs):
             config = DataConfig(data_dir="data/raw", batch_size=64, num_workers=workers)
             train_loader, _ = get_dataloaders(config)
-            
+
             it = iter(train_loader)
             next(it)
-            
+
             start = time.time()
             for i, batch in enumerate(it):
-                if i >= batches_to_check: break
+                if i >= batches_to_check:
+                    break
             end = time.time()
             times.append(end - start)
-            
+
         mean_time = np.mean(times)
         std_time = np.std(times)
         final_means.append(mean_time)
@@ -37,16 +39,17 @@ def benchmark():
 
     # --- Plotting ---
     plt.figure(figsize=(10, 6))
-    plt.errorbar(worker_counts, final_means, yerr=final_stds, fmt='-o', capsize=5, ecolor='red', label='Timing')
-    plt.xlabel('Number of Workers')
-    plt.ylabel(f'Time (s) for {batches_to_check} batches')
-    plt.title('M29: DataLoader Performance (Mean & Std Dev)')
-    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.errorbar(worker_counts, final_means, yerr=final_stds, fmt="-o", capsize=5, ecolor="red", label="Timing")
+    plt.xlabel("Number of Workers")
+    plt.ylabel(f"Time (s) for {batches_to_check} batches")
+    plt.title("M29: DataLoader Performance (Mean & Std Dev)")
+    plt.grid(True, linestyle="--", alpha=0.7)
     plt.legend()
-    
+
     plot_path = "reports/figures/dataloader_performance.png"
     plt.savefig(plot_path)
     print(f"\n Success! Plot saved to: {plot_path}")
+
 
 if __name__ == "__main__":
     benchmark()
