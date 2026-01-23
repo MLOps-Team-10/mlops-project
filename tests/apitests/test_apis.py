802e0c78cb4f00154e18130dcc847f228e946b91
Marco <marco.baresi01@gmail.com>
2026-01-23 17:10:35 +0100

    fix: solve formatting issue

--- tests/apitests/test_apis.py
+++ tests/apitests/test_apis.py
@@ -4,10 +4,12 @@ from eurosat_classifier.api import app
 
 client = TestClient(app)
 
+
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json()["status"] == "ok"
+
 
 def test_predict_forest():
     """
@@ -18,19 +20,17 @@ def test_predict_forest():
     assert os.path.exists(file_path), f"File not found: {file_path}"
 
     with open(file_path, "rb") as f:
-        response = client.post(
-            "/predict",
-            files={"file": ("Forest_1.jpg", f, "image/jpeg")}
-        )
+        response = client.post("/predict", files={"file": ("Forest_1.jpg", f, "image/jpeg")})
 
     assert response.status_code == 200
     data = response.json()
-    
+
     assert data["filename"] == "Forest_1.jpg"
     assert "prediction" in data
     # Optional: Check if the model actually predicted Forest
-    # assert data["prediction"] == "Forest" 
+    # assert data["prediction"] == "Forest"
     assert data["meta_max_length"] == 10  # Default value
+
 
 def test_predict_river_custom_param():
     """
@@ -43,12 +43,12 @@ def test_predict_river_custom_param():
         response = client.post(
             "/predict",
             params={"max_length": 42},  # Testing the query parameter
-            files={"file": ("River_1.jpg", f, "image/jpeg")}
+            files={"file": ("River_1.jpg", f, "image/jpeg")},
         )
 
     assert response.status_code == 200
     data = response.json()
-    
+
     assert data["filename"] == "River_1.jpg"
     assert "prediction" in data
     assert data["meta_max_length"] == 42
